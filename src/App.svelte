f4c1d6003ec63425cf6e7fddf0ed31088638be08
Phil Reif <philipp.reif@gandalan.de>
2022-10-26 16:57:53 +0200

    Switch to svelte-routing, discard svelte-spa-router

--- src/App.svelte
+++ src/App.svelte
@@ -3,20 +3,15 @@
   import Auth from './lib/Auth.svelte';
   import Counter from './lib/Counter.svelte';
   import IdasStatus from './lib/IDASStatus.svelte';
-  import Router, { link } from 'svelte-spa-router'
-
-  const routes = {
-    // Home
-    '/': IdasStatus,
-    // Auth
-    '/auth*': Auth,
-    // Counter test
-    '/counter': Counter,
-  }
+  import { Router, Route } from 'svelte-routing';
 </script>
 
 <main>
   <img src={logo} alt="Svelte Logo" class="w-16" />
 
-  <Router {routes} />
+  <Router>
+    <Route path="/auth*" component={Auth} />
+    <Route path="/counter" component={Counter} />
+    <Route component={IdasStatus} />
+  </Router>
 </main>
