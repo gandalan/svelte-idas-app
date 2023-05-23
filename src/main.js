943ccdeacc6e0b1ac9de9c5adb8c8b4f39d66901
Adam Stachowicz <adam.stachowicz@fingo.info>
2023-05-23 10:26:28 +0200

    Remove wwwroot folder. Revert build fix

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import App from "./App.svelte";
 import "./index.css";
 import { initIDAS } from "./auth";
 
-const settings = await initIDAS("<INSERT YOUR AUTH TOKEN HERE>");
+const settings = await initIDAS(<INSERT YOUR AUTH TOKEN HERE>);
 
 const app = new App({
   target: document.getElementById("app"),
